9913b09c92f5a2b767d7a9791a306621eb7bd35c
Reenu Sharma <reenu.sharma_cs19@gla.ac.in>
2023-11-26 00:41:17 +0530

    Time: 3 ms (31.97%) | Memory: 6.4 MB (16.50%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,16 +1,29 @@
+// class Solution {
+// public:
+//     int minBitFlips(int start, int goal) {
+        
+//         int a = (start ^ goal); // this will do xor of given numbers
+//            //now count set bits
+//         int count = 0; 
+//         while(a){
+//             if(a&1){
+//                 count++;
+//             }
+//         a = a>>1;  // this is right shift operator
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        
-        int a = (start ^ goal); // this will do xor of given numbers
-           //now count set bits
-        int count = 0; 
-        while(a){
-            if(a&1){
-                count++;
-            }
-        a = a>>1;  // this is right shift operator
+        int ans=0;
+        string s= bitset<32> (start).to_string();
+        string str= bitset<32> (goal).to_string();
+        for(int i=0;i<32;i++){
+            if(s[i]!=str[i]) ans++;
         }
-        return count;
+        return ans;
     }
 };
